84d963a3d1089a2840f7ec56109c0daa7c650aad
Germanins6 <1996GInsua@gmail.com>
2020-09-28 19:56:01 +0100

    Loading libraries properly

--- ZeroEngine/Core/Main.cpp
+++ ZeroEngine/Core/Main.cpp
@@ -3,8 +3,9 @@
 #include "Globals.h"
 
 #include "SDL/include/SDL.h"
-#pragma comment( lib,"SDL/libx86/SDL2.lib" )
-#pragma comment( lib,"SDL/libx86/SDL2main.lib" )
+
+#pragma comment( lib,"Core/SDL/libx86/SDL2.lib" )
+#pragma comment( lib,"Core/SDL/libx86/SDL2main.lib" )
 
 enum main_states
 {
