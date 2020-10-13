eea2da8f94356a4802c548da4fc6aea8077d7437
Germán Insua <1996GInsua@gmail.com>
2020-10-13 11:36:28 +0100

    Base template to develop window buffer render

---
+++ ZeroEngine/Core/ViewportBuffer.cpp
@@ -0,0 +1,41 @@
+#include "ViewportBuffer.h"
+#include "Application.h"
+#include "Globals.h"
+
+#include <string>
+#include "ImGui/imgui_internal.h"
+#include <gl/GL.h>
+
+//quemepario
+ViewportBuffer::ViewportBuffer(Application* app, bool start_enabled) : Module(app, start_enabled){
+	show_viewport_window = true;
+}
+
+//Lapuitra
+ViewportBuffer::~ViewportBuffer()
+{}
+
+bool  ViewportBuffer::Init() {
+	return true;
+}
+
+bool ViewportBuffer::Start() {
+	return true;
+}
+
+update_status ViewportBuffer::PreUpdate(float dt) {
+
+	return UPDATE_CONTINUE;
+}
+
+update_status ViewportBuffer::Update(float dt) {
+	return UPDATE_CONTINUE;
+}
+
+update_status ViewportBuffer::PostUpdate(float dt) {
+	return UPDATE_CONTINUE;
+}
+
+bool ViewportBuffer::CleanUp() {
+	return true;
+}
---
+++ ZeroEngine/Core/ViewportBuffer.h
@@ -0,0 +1,28 @@
+#include "Module.h"
+#include "Globals.h"
+
+#include "glew/include/glew.h"
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_impl_sdl.h"
+#include "ImGui/imgui_impl_opengl3.h"
+#include <string>
+
+class ViewportBuffer : public Module
+{
+public:
+	ViewportBuffer(Application* app, bool start_enabled = true);
+	~ViewportBuffer();
+
+	bool Init();
+	bool Start();
+	update_status PreUpdate(float dt);
+	update_status Update(float dt);
+	update_status PostUpdate(float dt);
+	bool CleanUp();
+	
+
+public:
+
+	bool show_viewport_window;
+
+};
