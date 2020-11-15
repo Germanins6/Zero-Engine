826fdcdfed8e18592dac00ce5762797212a590d4
christianpi4 <chricraft4100@gmail.com>
2020-11-15 18:38:18 +0100

    Compoenent Transform (WIP)

--- ZeroEngine/Core/ComponentTransform.cpp
+++ ZeroEngine/Core/ComponentTransform.cpp
@@ -1,13 +1,16 @@
 #include "ComponentTransform.h"
-
 #include "glew/include/glew.h"
 
-ComponentTransform::ComponentTransform(GameObject* parent) : Component(parent, ComponentType::TRANSFORM) {
+ComponentTransform::ComponentTransform(GameObject* parent, Mesh* data) : Component(parent, ComponentType::TRANSFORM) {
 	
-	position = { 0.0f, 0.0f, 0.0f };
-	rotation = { 0.0f, 0.0f, 0.0f };
-	scale = { 1.0f , 1.0f , 1.0f };
+	info = new TransformData({ 0,0,0 }, { 0,0,0 }, { 1,1,1 }, {0,0,0,0});
+
+	if (data != nullptr) {
+		data->resource_mesh->transform = info;
+	}
+	
 }
+
 
 ComponentTransform::~ComponentTransform() {
 
@@ -15,18 +18,49 @@ ComponentTransform::~ComponentTransform() {
 
 bool ComponentTransform::Update(float dt) {
 	//LOG("Transform update");
-	glTranslatef(position.x, position.y, position.z);
+	
 	return true;
 }
 
-void ComponentTransform::SetPosition() {
+void TransformData::SetPosition(float x, float y, float z) {
+	
+	position.x = x;
+	position.y = y;
+	position.z = z;
 
 }
 
-void ComponentTransform::SetRotation() {
+void TransformData::SetRotation(float x, float y, float z) {
+		
+	euler = float3(x, y, z);
+	rotation = Quat::FromEulerXYZ(x * DEGTORAD, y * DEGTORAD, z * DEGTORAD);
 
 }
 
-void ComponentTransform::SetScale() {
+void TransformData::SetScale(float x, float y, float z) {
+	
+	scaling.x = x;
+	scaling.y = y;
+	scaling.z = z;
 
+}
+
+TransformData* ComponentTransform::GetTransform() {
+
+	return info;
+
+}
+
+float4x4 TransformData::GetLocalTransform() {
+
+	local_transform = float4x4::FromTRS(position, rotation, scaling);
+
+	return local_transform;
+}
+
+float4x4 TransformData::GetGlobalTransform() {
+
+	global_transform = local_transform;
+
+	return global_transform;
 }
--- ZeroEngine/Core/ComponentTransform.h
+++ ZeroEngine/Core/ComponentTransform.h
@@ -3,25 +3,48 @@
 #include "Component.h"
 #include "MathGeoLib/include/MathGeoLib.h"
 
+#include "Globals.h"
+
+#include "ComponentMesh.h"
+#include "Assimp/include/vector3.h"
+#include "Assimp/include/quaternion.h"
+
+struct TransformData {
+
+	TransformData(float3 pos, float3 eu, float3 scale, Quat rot) : position{ pos }, euler{ eu }, scaling{ scale }, rotation{ rot } {}
+	~TransformData() {}
+	float3 position, euler, scaling;
+	Quat rotation;
+
+	void SetPosition(float x, float y, float z);
+	void SetRotation(float x, float y, float z);
+	void SetScale(float x, float y, float z);
+
+	float4x4 GetLocalTransform();
+	float4x4 GetGlobalTransform();
+
+	float4x4 local_transform = float4x4::identity;
+	float4x4 global_transform = float4x4::identity;
+};
+
 class ComponentTransform : public Component {
 
 public:
 
-	ComponentTransform(GameObject* parent);
+	ComponentTransform(GameObject* parent, Mesh* data);
 	~ComponentTransform();
 
 	bool Update(float dt);
 
-	void SetPosition();
-	void SetRotation();
-	void SetScale();
+	TransformData* GetTransform();
+	
 
 public:
-	
-	vec3 position;
-	Quat rotation;
-	float3 scale;
 
 	float4x4 localMatrix;
 	float4x4 globalMatrix;
+
+
+	TransformData* info;
+
 };
